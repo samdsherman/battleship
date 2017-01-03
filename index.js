4e3805b2eaa3d925703901635cded4ca3fcf932b
sam sherman <samdsherman@gmail.com>
2017-01-03 15:21:31 -0800

    make loop ask each player for ship placement

--- index.js
+++ index.js
@@ -34,83 +34,85 @@ const rowMap = {
 const cols = Object.keys(colMap);
 const rows = Object.keys(rowMap);
 
-let player1 = [];
-let player2 = [];
+let players = [[], []];
 
 for (let i = 0; i < rows.length; ++i) {
-  player1.push([]);
-  player2.push([]);
+  players[0].push([]);
+  players[1].push([]);
   for (let j = 0; j < cols.length; ++j) {
-    player1[i].push(null);
-    player2[i].push(null);
+    players[0][i].push(null);
+    players[1][i].push(null);
   }
 }
 
-for (let ship in ships) {
-  console.log('Player 1 ships:');
-  utils.printShipsGrid(player1, rows, cols);
+players.forEach((player, index) => {
+  for (let ship in ships) {
+    console.log('Player ' + index + ' ships:');
+    utils.printShipsGrid(player, rows, cols);
 
-  console.log('Player 1, place your ' + ship + '(' + ships[ship] + ' units)');
-  console.log('Enter start and end coordinates in the format "A5"');
-  while (true) {
-    let start = utils.ask('Start? ');
-    let end = utils.ask('End? ');
+    console.log('Player ' + index + ', place your ' + ship + '(' + ships[ship] + ' units)');
+    console.log('Enter start and end coordinates in the format "A5"');
+    while (true) {
+      let start = utils.ask('Start? ');
+      let end = utils.ask('End? ');
 
-    if (start.length !== 2 || end.length !== 2) { // problem with extensibility
-      console.log('Invalid coordinate format. Should look like "D7"');
-      continue;
-    }
+      if (start.length !== 2 || end.length !== 2) { // problem with extensibility
+        console.log('Invalid coordinate format. Should look like "D7"');
+        continue;
+      }
 
-    let startCol = colMap[start.charAt(0).toUpperCase()];
-    let endCol = colMap[end.charAt(0).toUpperCase()];
-    let startRow = rowMap[start.charAt(1)];
-    let endRow = rowMap[end.charAt(1)];
+      let startCol = colMap[start.charAt(0).toUpperCase()];
+      let endCol = colMap[end.charAt(0).toUpperCase()];
+      let startRow = rowMap[start.charAt(1)];
+      let endRow = rowMap[end.charAt(1)];
 
-    if (startCol === undefined || endCol === undefined) {
-      console.log('Invalid column. Valid options are ' + cols[0] + ' through ' + cols[cols.length - 1] + '.');
-      continue;
-    }
-    if (startRow === undefined || endRow === undefined) {
-      console.log('Invalid row. Valid options are ' + rows[0] + ' through ' + rows[rows.length - 1] + '.');
-      continue;
-    }
-    if (startCol !== endCol && startRow !== endRow) {
-      console.log('Invalid placement. Ship must lie on a row or a column, no diagonals please.');
-      continue;
-    }
-    if (Math.abs(startCol - endCol) + 1 !== ships[ship] && Math.abs(startRow - endRow) + 1 !== ships[ship]) {
-      console.log('Invalid size. ' + ship + ' is ' + ships[ship] + ' units long.');
-      continue;
-    }
+      if (startCol === undefined || endCol === undefined) {
+        console.log('Invalid column. Valid options are ' + cols[0] + ' through ' + cols[cols.length - 1] + '.');
+        continue;
+      }
+      if (startRow === undefined || endRow === undefined) {
+        console.log('Invalid row. Valid options are ' + rows[0] + ' through ' + rows[rows.length - 1] + '.');
+        continue;
+      }
+      if (startCol !== endCol && startRow !== endRow) {
+        console.log('Invalid placement. Ship must lie on a row or a column, no diagonals please.');
+        continue;
+      }
+      if (Math.abs(startCol - endCol) + 1 !== ships[ship] && Math.abs(startRow - endRow) + 1 !== ships[ship]) {
+        console.log('Invalid size. ' + ship + ' is ' + ships[ship] + ' units long.');
+        continue;
+      }
 
-    let invalid = false;
-    if (startCol === endCol) {
-      for (let i = Math.min(startRow, endRow); i <= Math.max(startRow, endRow); ++i) {
-        if (player1[i][startCol]) {
-          invalid = true;
+      let invalid = false;
+      if (startCol === endCol) {
+        for (let i = Math.min(startRow, endRow); i <= Math.max(startRow, endRow); ++i) {
+          if (player[i][startCol]) {
+            invalid = true;
+          }
+        }
+      } else {
+        for (let i = Math.min(startCol, endCol); i <= Math.max(startCol, endCol); ++i) {
+          if (player[startRow][i]) {
+            invalid = true;
+          }
         }
       }
-    } else {
-      for (let i = Math.min(startCol, endCol); i <= Math.max(startCol, endCol); ++i) {
-        if (player1[startRow][i]) {
-          invalid = true;
+      if (invalid) {
+        console.log('Invalid location. Must not overlap with a ship that has already been placed.');
+        continue;
+      }
+
+      if (startCol === endCol) {
+        for (let i = Math.min(startRow, endRow); i <= Math.max(startRow, endRow); ++i) {
+          player[i][startCol] = ship;
+        }
+      } else {
+        for (let i = Math.min(startCol, endCol); i <= Math.max(startCol, endCol); ++i) {
+          player[startRow][i] = ship;
         }
       }
+      break;
     }
-    if (invalid) {
-      console.log('Invalid location. Must not overlap with a ship that has already been placed.');
-      continue;
-    }
-
-    if (startCol === endCol) {
-      for (let i = Math.min(startRow, endRow); i <= Math.max(startRow, endRow); ++i) {
-        player1[i][startCol] = ship;
-      }
-    } else {
-      for (let i = Math.min(startCol, endCol); i <= Math.max(startCol, endCol); ++i) {
-        player1[startRow][i] = ship;
-      }
-    }
-    break;
   }
-}
+  utils.clearConsole();
+});
