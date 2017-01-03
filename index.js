36a384e429964f482310cf5a6b81151ce535763e
sam sherman <samdsherman@gmail.com>
2017-01-03 12:54:57 -0800

    add some constants

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const utils = require('./utils');
 
+const ships = {
+  Carrier: 5,
+  Battleship: 4,
+  Cruiser: 3,
+  Submarine: 3,
+  Destroyer: 2
+};
+
+const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+const rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log('testing');
+utils.clearConsole();
