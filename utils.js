392073656a5dbd590a740152fa6fe93a40608961
sam sherman <samdsherman@gmail.com>
2017-01-03 13:31:12 -0800

    add grid printing function

--- utils.js
+++ utils.js
@@ -15,3 +15,14 @@ module.exports.ask = function(...questions) {
 module.exports.clearConsole = function() {
   console.log('\x1Bc');
 };
+
+module.exports.printShipsGrid = function(grid, rows, cols) {
+  let top = '  |';
+  cols.forEach((col, index) => top += ' ' + col + ' |');
+  console.log(top);
+  rows.forEach((row, rowIndex) => {
+    let current = row + ' |';
+    cols.forEach((col, colIndex) => current += ' ' + (grid[rowIndex][colIndex] ? 'X' : ' ') + ' |');
+    console.log(current);
+  });
+};
