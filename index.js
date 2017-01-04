abcef3cc693f7920fb3e6e21400eb433ecf2ccb3
sam sherman <samdsherman@gmail.com>
2017-01-03 18:09:40 -0800

    Complete basic functionality

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const utils = require('./utils');
 
 const ships = {
-  // Carrier: 5,
-  // Battleship: 4,
-  // Cruiser: 3,
-  // Submarine: 3,
+  Carrier: 5,
+  Battleship: 4,
+  Cruiser: 3,
+  Submarine: 3,
   Destroyer: 2
 };
 const colMap = {
@@ -35,6 +35,11 @@ const cols = Object.keys(colMap);
 const rows = Object.keys(rowMap);
 
 let players = [
+  utils.nullMatrix(rows.length, cols.length),
+  utils.nullMatrix(rows.length, cols.length)
+];
+
+let guesses = [
   utils.nullMatrix(rows.length, cols.length),
   utils.nullMatrix(rows.length, cols.length)
 ];
@@ -108,3 +113,48 @@ players.forEach((player, index) => {
 });
 
 let currentTurn = 0;
+let nextTurn = 1;
+let playerShips = [{}, {}];
+let gameOver = false;
+while (true) {
+  utils.printGuessGrid(guesses[currentTurn], rows, cols);
+  console.log('Player ' + currentTurn + ', enter coordinates to attack: ');
+  while (true) {
+    let guess = utils.ask('Target? ');
+    if (!utils.verifyCoordinates(guess, rowMap, colMap, rows, cols)) {
+      continue;
+    }
+    let guessCol = colMap[guess.charAt(0).toUpperCase()];
+    let guessRow = rowMap[guess.charAt(1)];
+    if (guesses[currentTurn][guessRow][guessCol]) {
+      console.log('Already guessed that space. Guess a new space.');
+      continue;
+    }
+
+    let target = players[nextTurn][guessRow][guessCol];
+    if (target) {
+      guesses[currentTurn][guessRow][guessCol] = 'X';
+      playerShips[nextTurn][target] = (playerShips[nextTurn][target] || 0) + 1;
+      utils.printGuessGrid(guesses[currentTurn], rows, cols);
+      if (playerShips[nextTurn][target] === ships[target]) {
+        console.log('Sunk opposing ' + target + '!');
+        if (utils.eql(playerShips[nextTurn], ships)) {
+          console.log('Player ' + currentTurn + ' wins!');
+          gameOver = true;
+        }
+      } else {
+        console.log('Hit!');
+      }
+    } else {
+      guesses[currentTurn][guessRow][guessCol] = 'O';
+      utils.printGuessGrid(guesses[currentTurn], rows, cols);
+      console.log('Miss!');
+    }
+    break;
+  }
+  if (gameOver) {
+    break;
+  }
+  utils.ask('Press enter to continue.');
+  [currentTurn, nextTurn] = [nextTurn, currentTurn];
+}
--- utils.js
+++ utils.js
@@ -19,6 +19,17 @@ module.exports.printShipsGrid = function(grid, rows, cols) {
   });
 };
 
+module.exports.printGuessGrid = function(grid, rows, cols) {
+  let top = '  |';
+  cols.forEach((col, index) => top += ' ' + col + ' |');
+  console.log(top);
+  rows.forEach((row, rowIndex) => {
+    let current = row + ' |';
+    cols.forEach((col, colIndex) => current += ' ' + (grid[rowIndex][colIndex] || ' ') + ' |');
+    console.log(current);
+  });
+};
+
 module.exports.verifyCoordinates = function(coords, rowMap, colMap, rows, cols) {
   if (coords.length !== 2) {
     console.log('Invalid coordinate format. Should look like "D7"');
@@ -45,4 +56,27 @@ module.exports.nullMatrix = function(rows, cols) {
     }
   }
   return result;
+};
+
+module.exports.eql = function(obj1, obj2) {
+  if (obj1 === obj2) {
+    return true;
+  }
+  if (obj1 instanceof Object && obj2 instanceof Object) {
+    let obj1Keys = Object.keys(obj1);
+    let obj2Keys = Object.keys(obj2);
+
+    if (obj1Keys.length !== obj2Keys.length) {
+      return false;
+    }
+
+    for (let i = 0; i < obj1Keys.length; ++i) {
+      if (!module.exports.eql(obj1[obj1Keys[i]], obj2[obj1Keys[i]])) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 };
